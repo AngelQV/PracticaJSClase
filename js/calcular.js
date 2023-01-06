7958f5fbb0919caa3050f1e34904e1e1940777d8
AngelQV <ninovargas@hotmail.com>
2023-01-06 00:24:06 -0500

    Version 1

--- js/calcular.js
+++ js/calcular.js
@@ -1,15 +1,15 @@
 function calcular(){
-    let n1, n2, n3, promedio, ap="";
-    n1=document.getElementById("num1").value;
-    n2=document.getElementById("num2").value;
-    n3=document.getElementById("num3").value;
-    promedio=(parseFloat(n1)+parseFloat(n2)+parseFloat(n3))/3;
-    promedio=Math.round(promedio)
-    if(promedio<7){
-        ap="Reprobado";
-    }else{
-        ap="Aprobado";
+    let marca, modelo, descuento=0;
+    marca=document.getElementById("marca").value;
+    modelo=document.getElementById("modelo").value;
+    if((marca=="ford")&&(modelo=="fiesta")){
+        descuento=5;
     }
-    document.getElementById("prom").innerHTML=(`El promedio de notas es: ${promedio}`);
-    document.getElementById("apr").innerHTML=(`El estudiante ha sido ${ap}`);
+        else{if((marca=="ford")&&(modelo=="focus")){
+            descuento=10;
+        }else{
+            descuento="No tiene descuento";
+        }
+    }
+    document.getElementById("descuento").innerHTML=(`El descuento es de: ${descuento} %`);
 }
